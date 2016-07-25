abb5e5d27cadd571a2492e762a697cd7a0146b59
William <willtheorangeguy@outlook.com>
2016-07-25 12:20:45 -0700

    Update version and license

--- setup_information.py
+++ setup_information.py
@@ -4,10 +4,10 @@ import sys
 
 setup(
     name='Erin Hunter Book Chooser',
-    version='3.0',
+    version='3.1',
     author = 'William Vandergraaf',
     author_email = 'willtheorangeguy@outlook.com',
-    url = 'https://github.com/dog-face-development/erin-hunter-book-chooser',
-    license='MIT',
+    url = 'https://github.com/Dog-Face-Development/Erin-Hunter-Book-Chooser',
+    license='Apache 2.0',
     description= "This program will select you favorite Erin Hunter authored book based on you favorite of three animals.",
 )
